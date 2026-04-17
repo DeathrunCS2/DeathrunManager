8fae8ad2b1f7bb3879c1d9e8f1b215a18677ecf2
aquevadis <109932077+aquevadis@users.noreply.github.com>
2026-04-18 01:32:39 +0300

    docs: add more info for the api

--- src/DeathrunManager.Shared/DeathrunManager.Shared.cs
+++ src/DeathrunManager.Shared/DeathrunManager.Shared.cs
@@ -32,7 +32,26 @@ public interface IDeathrunManager
 
     #endregion
 
+    /// <summary>
+    /// Provides access to the configuration properties specific to the deathrun functionality of the
+    /// <see cref="IDeathrunManager"/> interface.
+    /// </summary>
+    /// <remarks>
+    /// This property encapsulates the configuration settings implemented by <see cref="IManagerBaseConfig"/>.
+    /// These settings include various gameplay behaviors and server adjustments such as enabling auto bunny hopping,
+    /// removing buy zones, setting round times, and more.
+    /// The exact behavior and values of these configurations are determined by the implementation of the
+    /// <see cref="IManagerBaseConfig"/> interface.
+    /// </remarks>
     public IManagerBaseConfig Config { get; }
-    
+
+    /// <summary>
+    /// Provides access to various subsystem managers within the <see cref="IDeathrunManager"/> interface.
+    /// </summary>
+    /// <remarks>
+    /// This property contains references to the key functional managers of the <see cref="IDeathrunManager"/> system,
+    /// such as player management, gameplay rules, administrative controls, and menu handling. These managers
+    /// facilitate modular and organized interactions within the deathrun environment.
+    /// </remarks>
     public IDeathrunManagers Managers { get; }
 }
