7d7ed0c07c623bb640a4f194e9b5bc503f696c8a
aquevadis <109932077+aquevadis@users.noreply.github.com>
2026-04-18 01:31:21 +0300

    docs: add info for IManagerBaseConfig getter

--- src/DeathrunManager.Shared/Config/IManagerConfig.cs
+++ src/DeathrunManager.Shared/Config/IManagerConfig.cs
@@ -2,5 +2,11 @@ namespace DeathrunManager.Shared.Config;
 
 public interface IManagerConfig
 {
+    /// <summary>
+    /// Property providing access to the base configuration for the deathrun manager.
+    /// </summary>
+    /// <value>
+    /// An implementation of <see cref="IManagerBaseConfig"/> containing the core configuration settings.
+    /// </value>
     IManagerBaseConfig GetBaseConfig { get; }
 }
