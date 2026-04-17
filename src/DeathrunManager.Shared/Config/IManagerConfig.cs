cac0926af5db308e51d95964b4d30ffe69650b27
aquevadis <109932077+aquevadis@users.noreply.github.com>
2026-04-18 01:30:28 +0300

    api: remove unused stuff

--- src/DeathrunManager.Shared/Config/IManagerConfig.cs
+++ src/DeathrunManager.Shared/Config/IManagerConfig.cs
@@ -4,18 +4,3 @@ public interface IManagerConfig
 {
     IManagerBaseConfig GetBaseConfig { get; }
 }
-
-public interface IDeathrunManagerConfigStructure
-{
-    bool GiveWeaponToCTs { get;}
-    bool RemoveBuyZones { get;}
-    bool RemoveMoneyFromGameAndHud { get;}
-    bool SetRoundTimeOneHour { get;}
-    bool EnableClippingThroughTeamMates { get;}
-    bool EnableAutoBunnyHopping { get;}
-    
-    bool EnableKillCommandForCTs { get;}
-    bool EnableKillCommandForTs { get;}
-
-    string Prefix { get; }
-}
